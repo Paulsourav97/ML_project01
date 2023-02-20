61090636210e29a4ebff45c73ac65e540ce03b88
Paulsourav97 <souravpaul888@gmail.com>
2023-02-21 01:36:10 +0530

    added

---
+++ housing/util/util.py
@@ -0,0 +1,12 @@
+import yaml
+from housing.exception import HousingException
+import os, sys
+
+"""reads yaml file and returns the content as dictionary"""
+
+def read_yaml_file(file_path: str)->dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e, sys) from e
